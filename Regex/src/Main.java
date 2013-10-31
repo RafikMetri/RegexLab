21aaa0b692eb52b2757c9903f45d9279bb1827a0
RafikMetri <rumetri@gmail.com>
2013-10-31 08:38:59 -0600

    Added file scanning

--- Regex/src/Main.java
+++ Regex/src/Main.java
@@ -1,3 +1,4 @@
+
 /**
  * Created with IntelliJ IDEA.
  * User: rmetri
@@ -5,6 +6,7 @@
  * Time: 1:18 AM
  * To change this template use File | Settings | File Templates.
  */
+import java.io.File;
 import java.util.regex.*;
 import java.util.*;
 
@@ -15,32 +17,23 @@ public class Main {
 
 
     public static void main(String[] args){
-        String text = "<html>\n" +
-                "<head>\\n" +
-                "    <meta content=\"text/html; charset=ISO-8859-1\"\n" +
-                "          http-equiv=\"content-type\">\n" +
-                "    <title>All About Fish</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<h1>All About Fish</h1>\n" +
-                "Fish are facinating animals that live under the\n" +
-                "<a href=\"http://www.ocean.com/\">sea</a>.\n" +
-                "They have <a href=\"http://www.fish.ocean.com/scales/scale.1.jpg\">scales</a>\n" +
-                "and <a href=\"http://marinebiology.org/fins//fin-0.GIF\">fins</a>.<br> There are many good\n" +
-                "<a href=\"http://www.seafood-recipes.com/directory/salmon.asp\">recipes</a> to cook fish.<br>\n" +
-                "Fish make excellent <a href=\"http://www.petfish.net/index.php\">pets</a>,\n" +
-                "and many people are interested in <a href=\"http://www.tropfish.com\">tropical fish</a>.\n" +
-                "My favorite fish-related website is http://www.fish.com/tropical.<br>\n" +
-                "Some people think that \"http://fish/fish.html\" would be a good URL, but they are wrong!\n" +
-                "Others think that starfish should have a URL of \"http://fish.com/*fish\" but that's bad too!\n" +
-                "<br>\n" +
-                "</body>\n" +
-                "</html>";
+        Scanner read = null;
+        String text = null;
+
+        try{
+            read = new Scanner(new File("Fish.txt"));
+            read.useDelimiter("$");
+            text = read.next();
+            read.close();
+        }
+        catch(Exception e){
+            System.out.println("There was a problem scanning your file");
+        }
+
         printUrls(text);
     }
 
     public static void printUrls(String text){
-        //System.out.println(Arrays.toString(compile("\"(https?://)?(((\\w|-|-_)+\\.)?){2}(((\\w|-|-_)+\\.)?)(([A-Z]|[a-z]){3})(/((\\w|-|-_|.)?))?\"").split(text)));
         Pattern regex = Pattern.compile("\"(https?://)?(((\\w|-|-_)+\\.)?){2}(((\\w|-|-_)+\\.)?)(([A-Z]|[a-z]){3})(/((\\w|-|-_|.))+)?\\\"", Pattern.CASE_INSENSITIVE);
         Matcher search = regex.matcher(text);
         int count = 0;
